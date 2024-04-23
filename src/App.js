8493fb3fdb7b219d83f5202e170069ec4384bd55
Vincent Do <vincentvietdo@gmail.com>
2024-04-22 21:24:18 -0500

    Initial commit with React and MUI setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello TPEO!</p>
+        <Button variant="contained" color="primary">
+          Test Button
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
